ed8bf19b4e9c4edbc208adcfde36e0b6da9ad265
Caroline Tan <caroline.kh.tan@gmail.com>
2021-02-17 23:03:25 +0800

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'tasks', to: 'tasks#index'
   # get 'tasks/new', to: 'tasks#new', as: 'new_task'
-  # get 'task/:id', to: 'tasks#show', as: 'task'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
   # post 'tasks', to: 'tasks#create'
-  # get 'task/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  # patch 'task/:id', to: 'tasks#update'
-  # delete 'task/:id', to: 'tasks#destroy', as: 'destroy_task'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
   resources :tasks
 end
